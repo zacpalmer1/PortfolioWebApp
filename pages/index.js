2d04005b7fc95dd78091897f76b12cd4ecdcad7e
Zac Palmer <palmerzt@g.cofc.edu>
2023-04-21 20:25:06 -0400

    changes to home page

--- pages/index.js
+++ pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
   <div className="w-screen h-screen flex flex-col lg:flex-col bg-stone-900">      
     <div className="mx-auto z-20 ">
       <div className="max-w-screen mx-auto flex flex-col z-20 items-center">
-        <h2 className="lg:text-8xlw-screen text-center md:text-8xl sm:text-8xl text-stone-600 hover:text-blue-400 duration-500 pt-48 z-20">Zac Palmer</h2>
+        <h2 className="lg:text-8xlw-screen text-center md:text-8xl sm:text-8xl text-stone-600 hover:text-blue-400 duration-500 pt-32 z-20">Zac Palmer</h2>
         <p className="lg:text-3xl md:text-3xl sm:text-3xl text-stone-600 hover:text-orange-400 duration-500 p-5  z-20">Software Engineer</p>
         <p className="lg:text-3xl md:text-3xl sm:text-3xl text-stone-600 hover:text-green-400 duration-500 pt-48 pb-3 z-20 ">Projects</p>
       </div>
@@ -38,18 +38,18 @@ export default function Home() {
         <Nav.Link
           href="/profile"
           >
-          <div className=" bg-stone-600 hover:text-blue-400 text-stone-900 duration-300 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:shadow-lg text-center w-64 h-18 m-4 rounded-[8px] z-20">
+          <div className=" bg-stone-600 hover:text-blue-400 text-stone-900 duration-300 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:shadow-lg text-center w-40 h-18 m-4 rounded-[8px] z-20">
             <div className="p-1">
-          <p className="m-3 text-xs">Interloop Scoreboard</p>
+          <p className="m-3 text-xs">Interloop </p>
           </div>
           </div>
           </Nav.Link>
           <Nav.Link
           href="/dashboard"
           >
-          <div className="bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-red-400 text-stone-900 duration-300 hover:shadow-lg text-center w-64 h-18 m-4 rounded-[8px] z-20">
+          <div className="bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-red-400 text-stone-900 duration-300 hover:shadow-lg text-center w-40 h-18 m-4 rounded-[8px] z-20">
             <div className=" p-1">
-                <p className="m-3  text-xs">Student Health</p>
+                <p className="m-3  text-xs">Health</p>
             </div>
           </div>
           </Nav.Link>
@@ -57,7 +57,7 @@ export default function Home() {
         <Nav.Link
           href="/profile"
           >
-          <div className=" bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-orange-400 text-stone-900  duration-300 hover:shadow-lg text-center w-64 h-18 m-4 rounded-[8px] z-20">
+          <div className=" bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-orange-400 text-stone-900  duration-300 hover:shadow-lg text-center w-40 h-18 m-4 rounded-[8px] z-20">
             <div className="p-1">
           <p className="m-3  text-xs">Golden</p>
           </div>
@@ -68,7 +68,7 @@ export default function Home() {
         <Nav.Link
           href="/profile"
           >
-          <div className=" bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-green-400 text-stone-900  duration-300 hover:shadow-lg text-center w-64 h-18 m-4 rounded-[8px] z-20">
+          <div className=" bg-stone-600 shadow-[0_25px_50px_-3px_rgba(0,0,0,0.4)] hover:text-green-400 text-stone-900  duration-300 hover:shadow-lg text-center w-40 h-18 m-4 rounded-[8px] z-20">
             <div className="p-1">
           <p className="m-3  text-xs">Android</p>
           </div>
